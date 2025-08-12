7761926ce28fcf7743d85079438d752cfae00278
AbyssalDragon123 <alexander2004deleon@gmail.com>
2025-08-12 13:16:05 -0600

    creacion de WebSite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Product } from './pages/product/product';
+import { Contact } from './pages/contact/contact';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'product', component: Product },
+    { path: 'contact', component: Contact },
+    { path: 'nosotros', component: About },
+    { path: '**', redirectTo: '/home' }
+];
